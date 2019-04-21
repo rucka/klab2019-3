b12cfbf1581cebdea67df1bcf83802c508b81a92
rucka <rucka@tiscalinet.it>
2019-04-21 14:57:01 +0200

    implement validation in vanilla fp

--- web/src/2-vanillafp/async.ts
+++ web/src/2-vanillafp/async.ts
@@ -17,104 +17,12 @@ export type Future<A> = {
 
 export const isSuccess = <A>(arg: any): arg is Success<A> =>
   arg.type === 'success'
-export const isFailure = <E>(arg: any): arg is Failure => arg.type === 'failure'
+export const isFailure = <_E>(arg: any): arg is Failure =>
+  arg.type === 'failure'
 
 const success = <A>(a: A): Success<A> => <Success<A>>{ a: a, type: 'success' }
 const fail = (e: string): Failure => <Failure>{ error: e, type: 'failure' }
 
-export function bind<A>(f: (cb: Callback<A>) => void): Future<A> {
-  return { type: 'future', f: f }
-}
-
-export function map<A, B>(a: Future<A>, f: (a: A) => B): Future<B> {
-  const fB = function(cb: Callback<B>) {
-    a.f((e: string | null, a: A | null) => {
-      if (e === null) cb(e, null)
-      if (a) cb(null, f(a))
-      cb('invalid error', null)
-    })
-  }
-  return bind<B>(fB)
-}
-export function foreach<A>(a: Future<A>, f: (a: A) => void): void {
-  a.f((e: string | null, a: A | null) => {
-    if (a) f(a)
-  })
-}
-export function flatmap<A, B>(a: Future<A>, f: (a: A) => Future<B>): Future<B> {
-  const fB = function(cbB: Callback<B>) {
-    a.f((eA: string | null, a: A | null) => {
-      if (eA === null) return cbB(eA, null)
-      if (a) {
-        const futB: Future<B> = f(a)
-        futB.f((eB: string | null, b: B | null) => {
-          if (eB === null) return cbB(eB, null)
-          cbB(null, b)
-        })
-      }
-    })
-  }
-  return bind<B>(fB)
-}
-export function for2<A1, A2, B>(
-  a1: Future<A1>,
-  a2: Future<A2>,
-  f: (a: A1, b: A2) => B
-): Future<B> {
-  const fB = function(cbB: Callback<B>) {
-    a1.f((e1, a1) => {
-      if (e1 === null) return cbB(e1, null)
-      a2.f((e2, a2) => {
-        if (e2 === null) return cbB(e2, null)
-        if (a1 && a2) return cbB(null, f(a1, a2))
-      })
-    })
-  }
-  return bind<B>(fB)
-}
-export function for3<A1, A2, A3, B>(
-  a1: Future<A1>,
-  a2: Future<A2>,
-  a3: Future<A3>,
-  f: (a: A1, b: A2, a3: A3) => B
-): Future<B> {
-  return for2(a1, for2(a2, a3, (a2, a3) => <[A2, A3]>[a2, a3]), (a1, rest) =>
-    f(a1, rest[0], rest[1])
-  )
-}
-export function for4<A1, A2, A3, A4, B>(
-  a1: Future<A1>,
-  a2: Future<A2>,
-  a3: Future<A3>,
-  a4: Future<A4>,
-  f: (a: A1, b: A2, a3: A3, a4: A4) => B
-): Future<B> {
-  return for2(
-    a1,
-    for3(a2, a3, a4, (a2, a3, a4) => <[A2, A3, A4]>[a2, a3, a4]),
-    (a1, rest) => f(a1, rest[0], rest[1], rest[2])
-  )
-}
-export function for5<A1, A2, A3, A4, A5, B>(
-  a1: Future<A1>,
-  a2: Future<A2>,
-  a3: Future<A3>,
-  a4: Future<A4>,
-  a5: Future<A5>,
-  f: (a: A1, b: A2, a3: A3, a4: A4, a5: A5) => B
-): Future<B> {
-  return for2(
-    a1,
-    for4(
-      a2,
-      a3,
-      a4,
-      a5,
-      (a2, a3, a4, a5) => <[A2, A3, A4, A5]>[a2, a3, a4, a5]
-    ),
-    (a1, rest) => f(a1, rest[0], rest[1], rest[2], rest[3])
-  )
-}
 export function onComplete<A>(
   a: Future<A>,
   f: (r: FutureResult<A>) => void
@@ -124,4 +32,102 @@ export function onComplete<A>(
     if (a !== null) return f(success(a))
     return fail('invalid operation: both error and argument cannot be null')
   })
+}
+export namespace Async {
+  export function bind<A>(f: (cb: Callback<A>) => void): Future<A> {
+    return { type: 'future', f: f }
+  }
+
+  export function map<A, B>(a: Future<A>, f: (a: A) => B): Future<B> {
+    const fB = function(cb: Callback<B>) {
+      a.f((e: string | null, a: A | null) => {
+        if (e === null) cb(e, null)
+        if (a) cb(null, f(a))
+        cb('invalid error', null)
+      })
+    }
+    return bind<B>(fB)
+  }
+  export function foreach<A>(a: Future<A>, f: (a: A) => void): void {
+    a.f((_: string | null, a: A | null) => {
+      if (a) f(a)
+    })
+  }
+  export function flatmap<A, B>(
+    a: Future<A>,
+    f: (a: A) => Future<B>
+  ): Future<B> {
+    const fB = function(cbB: Callback<B>) {
+      a.f((eA: string | null, a: A | null) => {
+        if (eA === null) return cbB(eA, null)
+        if (a) {
+          const futB: Future<B> = f(a)
+          futB.f((eB: string | null, b: B | null) => {
+            if (eB === null) return cbB(eB, null)
+            cbB(null, b)
+          })
+        }
+      })
+    }
+    return bind<B>(fB)
+  }
+  export function for2<A1, A2, B>(
+    a1: Future<A1>,
+    a2: Future<A2>,
+    f: (a: A1, b: A2) => B
+  ): Future<B> {
+    const fB = function(cbB: Callback<B>) {
+      a1.f((e1, a1) => {
+        if (e1 === null) return cbB(e1, null)
+        a2.f((e2, a2) => {
+          if (e2 === null) return cbB(e2, null)
+          if (a1 && a2) return cbB(null, f(a1, a2))
+        })
+      })
+    }
+    return bind<B>(fB)
+  }
+  export function for3<A1, A2, A3, B>(
+    a1: Future<A1>,
+    a2: Future<A2>,
+    a3: Future<A3>,
+    f: (a: A1, b: A2, a3: A3) => B
+  ): Future<B> {
+    return for2(a1, for2(a2, a3, (a2, a3) => <[A2, A3]>[a2, a3]), (a1, rest) =>
+      f(a1, rest[0], rest[1])
+    )
+  }
+  export function for4<A1, A2, A3, A4, B>(
+    a1: Future<A1>,
+    a2: Future<A2>,
+    a3: Future<A3>,
+    a4: Future<A4>,
+    f: (a: A1, b: A2, a3: A3, a4: A4) => B
+  ): Future<B> {
+    return for2(
+      a1,
+      for3(a2, a3, a4, (a2, a3, a4) => <[A2, A3, A4]>[a2, a3, a4]),
+      (a1, rest) => f(a1, rest[0], rest[1], rest[2])
+    )
+  }
+  export function for5<A1, A2, A3, A4, A5, B>(
+    a1: Future<A1>,
+    a2: Future<A2>,
+    a3: Future<A3>,
+    a4: Future<A4>,
+    a5: Future<A5>,
+    f: (a: A1, b: A2, a3: A3, a4: A4, a5: A5) => B
+  ): Future<B> {
+    return for2(
+      a1,
+      for4(
+        a2,
+        a3,
+        a4,
+        a5,
+        (a2, a3, a4, a5) => <[A2, A3, A4, A5]>[a2, a3, a4, a5]
+      ),
+      (a1, rest) => f(a1, rest[0], rest[1], rest[2], rest[3])
+    )
+  }
 }
--- web/src/2-vanillafp/optional.ts
+++ web/src/2-vanillafp/optional.ts
@@ -14,77 +14,82 @@ export const isNone = <_A>(arg: any): arg is None => arg.type === 'none'
 const some = <A>(arg: A): Some<A> => <Some<A>>{ a: arg, type: 'some' }
 const none = () => <None>{ type: 'none' }
 
-export function bind<A>(arg: A | null): Option<A> {
-  if (arg !== null) return some(arg)
-  else return none()
-}
+export namespace Optional {
+  export function bind<A>(arg: A | null): Option<A> {
+    if (arg !== null) return some(arg)
+    else return none()
+  }
 
-export function map<A, B>(a: Option<A>, f: (a: A) => B): Option<B> {
-  return isSome(a) ? some(f(a.a)) : a
-}
-export function foreach<_E, A>(a: Option<A>, f: (a: A) => void): void {
-  if (isSome(a)) f(a.a)
-}
+  export function map<A, B>(a: Option<A>, f: (a: A) => B): Option<B> {
+    return isSome(a) ? some(f(a.a)) : a
+  }
+  export function foreach<_E, A>(a: Option<A>, f: (a: A) => void): void {
+    if (isSome(a)) f(a.a)
+  }
 
-export function flatmap<A, B>(a: Option<A>, f: (a: A) => Option<B>): Option<B> {
-  return isSome(a) ? f(a.a) : a
-}
-export function for2<A1, A2, B>(
-  a1: Option<A1>,
-  a2: Option<A2>,
-  f: (a: A1, b: A2) => B
-): Option<B> {
-  if (isNone(a1) && isNone(a2)) {
-    return a1
+  export function flatmap<A, B>(
+    a: Option<A>,
+    f: (a: A) => Option<B>
+  ): Option<B> {
+    return isSome(a) ? f(a.a) : a
   }
-  if (isSome(a1) && isNone(a2)) {
-    return a2
+  export function for2<A1, A2, B>(
+    a1: Option<A1>,
+    a2: Option<A2>,
+    f: (a: A1, b: A2) => B
+  ): Option<B> {
+    if (isNone(a1) && isNone(a2)) {
+      return a1
+    }
+    if (isSome(a1) && isNone(a2)) {
+      return a2
+    }
+    if (isNone(a1) && isNone(a2)) {
+      return none()
+    }
+    return flatmap(a2, aa2 => flatmap(a1, aa1 => some(f(aa1, aa2))))
   }
-  if (isNone(a1) && isNone(a2)) {
-    return none()
+  export function for3<A1, A2, A3, B>(
+    a1: Option<A1>,
+    a2: Option<A2>,
+    a3: Option<A3>,
+    f: (a: A1, b: A2, a3: A3) => B
+  ): Option<B> {
+    return for2(a1, for2(a2, a3, (a2, a3) => <[A2, A3]>[a2, a3]), (a1, rest) =>
+      f(a1, rest[0], rest[1])
+    )
   }
-  return flatmap(a2, aa2 => flatmap(a1, aa1 => some(f(aa1, aa2))))
-}
-export function for3<A1, A2, A3, B>(
-  a1: Option<A1>,
-  a2: Option<A2>,
-  a3: Option<A3>,
-  f: (a: A1, b: A2, a3: A3) => B
-): Option<B> {
-  return for2(a1, for2(a2, a3, (a2, a3) => <[A2, A3]>[a2, a3]), (a1, rest) =>
-    f(a1, rest[0], rest[1])
-  )
-}
-export function for4<_E, A1, A2, A3, A4, B>(
-  a1: Option<A1>,
-  a2: Option<A2>,
-  a3: Option<A3>,
-  a4: Option<A4>,
-  f: (a: A1, b: A2, a3: A3, a4: A4) => B
-): Option<B> {
-  return for2(
-    a1,
-    for3(a2, a3, a4, (a2, a3, a4) => <[A2, A3, A4]>[a2, a3, a4]),
-    (a1, rest) => f(a1, rest[0], rest[1], rest[2])
-  )
-}
-export function for5<A1, A2, A3, A4, A5, B>(
-  a1: Option<A1>,
-  a2: Option<A2>,
-  a3: Option<A3>,
-  a4: Option<A4>,
-  a5: Option<A5>,
-  f: (a: A1, b: A2, a3: A3, a4: A4, a5: A5) => B
-): Option<B> {
-  return for2(
-    a1,
-    for4(
-      a2,
-      a3,
-      a4,
-      a5,
-      (a2, a3, a4, a5) => <[A2, A3, A4, A5]>[a2, a3, a4, a5]
-    ),
-    (a1, rest) => f(a1, rest[0], rest[1], rest[2], rest[3])
-  )
+  export function for4<_E, A1, A2, A3, A4, B>(
+    a1: Option<A1>,
+    a2: Option<A2>,
+    a3: Option<A3>,
+    a4: Option<A4>,
+    f: (a: A1, b: A2, a3: A3, a4: A4) => B
+  ): Option<B> {
+    return for2(
+      a1,
+      for3(a2, a3, a4, (a2, a3, a4) => <[A2, A3, A4]>[a2, a3, a4]),
+      (a1, rest) => f(a1, rest[0], rest[1], rest[2])
+    )
+  }
+  export function for5<A1, A2, A3, A4, A5, B>(
+    a1: Option<A1>,
+    a2: Option<A2>,
+    a3: Option<A3>,
+    a4: Option<A4>,
+    a5: Option<A5>,
+    f: (a: A1, b: A2, a3: A3, a4: A4, a5: A5) => B
+  ): Option<B> {
+    return for2(
+      a1,
+      for4(
+        a2,
+        a3,
+        a4,
+        a5,
+        (a2, a3, a4, a5) => <[A2, A3, A4, A5]>[a2, a3, a4, a5]
+      ),
+      (a1, rest) => f(a1, rest[0], rest[1], rest[2], rest[3])
+    )
+  }
 }
